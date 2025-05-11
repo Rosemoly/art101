da53b3e35c57582d1fa713742225173f58063a71
Rosemoly <emrvalde@ucsc.edu>
2025-05-11 14:33:01 -0700

    added lab 9 and homepage updated

---
+++ lab9/js/lab.js
@@ -0,0 +1,25 @@
+// index.js - This simple JavaScript/jQuery script uses buttons to modify some elements on the page
+// Requirements: jQuery must be loaded for this script to work.
+// Author: Emily Valdez
+// Date: 10 May 2025
+
+$(document).ready(function() {
+    // add buttons
+    $("#challenge").append("<button id='button-challenge'>Make Special</button>");
+    $("#problems").append("<button id='button-problems'>Make Special</button>");
+    $("#results").append("<button id='button-results'>Make Special</button>");
+  
+    // add event listeners
+    $("#button-challenge").click(function(){
+      $("#challenge").toggleClass("special");
+    });
+  
+    $("#button-problems").click(function(){
+      $("#problems").toggleClass("special");
+    });
+  
+    $("#button-results").click(function(){
+      $("#results").toggleClass("special");
+    });
+  });
+  
