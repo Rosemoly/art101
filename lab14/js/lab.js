027abef797ef646e63ed58af89bdb4fc7dac77d7
Rosemoly <emrvalde@ucsc.edu>
2025-05-28 17:35:44 -0700

    fixed debugs and added lab 14

---
+++ lab14/js/lab.js
@@ -0,0 +1,6 @@
+// index.js - This simple JavaScript/jQuery script gets a value from an input field and outputs a modified version.
+// Requirements: jQuery must be loaded for this script to work.
+// Author: Emily Valdez
+// Date: 24 May 2025
+
+
