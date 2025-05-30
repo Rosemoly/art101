215ebfdbcf5e25b0e517b3745803ad9767281682
Rosemoly <emrvalde@ucsc.edu>
2025-05-29 22:07:44 -0700

    fixed lab 9

--- lab9/js/lab.js
+++ lab9/js/lab.js
@@ -3,23 +3,20 @@
 // Author: Emily Valdez
 // Date: 10 May 2025
 
-$(document).ready(function() {
-    // add buttons
-    $("#challenge").append("<button id='button-challenge'>Make Special</button>");
-    $("#problems").append("<button id='button-problems'>Make Special</button>");
-    $("#results").append("<button id='button-results'>Make Special</button>");
-  
-    // add event listeners
-    $("#button-challenge").click(function(){
-      $("#challenge").toggleClass("special");
-    });
-  
-    $("#button-problems").click(function(){
-      $("#problems").toggleClass("special");
-    });
-  
-    $("#button-results").click(function(){
-      $("#results").toggleClass("special");
-    });
+$(document).ready(function () {
+  // Append buttons
+  $("#challenge").append("<button id='button-challenge'>Make Special</button>");
+  $("#problems").append("<button id='button-problems'>Make Special</button>");
+  $("#results").append("<button id='button-results'>Make Special</button>");
+
+  // Add click listeners
+  $("#button-challenge").click(function () {
+    $("#challenge").toggleClass("special");
   });
-  
+  $("#button-problems").click(function () {
+    $("#problems").toggleClass("special");
+  });
+  $("#button-results").click(function () {
+    $("#results").toggleClass("special");
+  });
+});
